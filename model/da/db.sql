80c985f1f89b1b965e248605e5adf758f0623dfa
mohammadreza najafi <mrnajafid@gmail.com>
2024-03-29 18:20:20 +0430

    product.da first upload

--- model/da/db.sql
+++ model/da/db.sql
@@ -1,13 +1,13 @@
-create database mft;
+create database storage;
 
-create table mft.person(
+create table storage.person(
     id int primary key auto_increment,
     name varchar(30),
     family varchar(30),
     phone int
 );
 
-create table mft.book(
+create table storage.book(
     id int primary key auto_increment,
     name varchar(30),
     count smallint,
