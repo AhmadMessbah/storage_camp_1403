b7ed7e19263cd4e9ae2c6e2359b28edac6c99e3f
mohammadreza najafi <mrnajafid@gmail.com>
2024-03-29 18:10:18 +0430

    db.sql first update

--- model/da/db.sql
+++ model/da/db.sql
@@ -3,6 +3,16 @@ create database mft;
 create table mft.person(
     id int primary key auto_increment,
     name varchar(30),
-    family varchar(30)
+    family varchar(30),
+    phone int
 );
+
+create table mft.book(
+    id int primary key auto_increment,
+    name varchar(30),
+    count smallint,
+    price_b smallint,
+    price_s smallint
+);
+
 
