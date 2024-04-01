74b8bb4eae745b86d9b5880a7ba281d387ece132
thisfunny <mrnajafid@gmail.com>
2024-04-01 20:54:09 +0330

    database creation checked.

--- model/da/db.sql
+++ model/da/db.sql
@@ -1,13 +1,13 @@
-create database mft;
+create database storage;
 
-create table mft.person(
+create table storage.person(
     id int primary key auto_increment,
     name varchar(30),
     family varchar(30),
     phone int
 );
 
-create table mft.product(
+create table storage.product(
     id int primary key auto_increment,
     name varchar(30),
     count smallint,
